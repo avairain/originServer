bda0eeafb2d5586bf514b5eadbdd0a2a88833997
yaoxingyu <yaoxingyu@hydsoft.com>
2019-05-30 14:11:47 +0800

    node runtime error

--- src/databaseOperation/index.js
+++ src/databaseOperation/index.js
@@ -26,7 +26,7 @@ Operation.prototype.init = async function () {
 Operation.prototype.connect = async function () {
   console.log(2)
   await new Promise((res) => {
-    this.db || MongoClient.connect(url, { ...options }, (err, db) => {
+    this.db || MongoClient.connect(url, options, (err, db) => {
       console.log(3)
       if (err) throw err
       console.log("数据库已创建!")
